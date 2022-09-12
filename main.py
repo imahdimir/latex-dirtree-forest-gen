60967ca962ca9222f156d923804c9b2f4033d150
imahdimir <imahdimir@gmail.com>
2022-09-12 21:07:36 +0430

    v1

--- main.py
+++ main.py
@@ -2,36 +2,31 @@
 
     """
 
-import pandas as pd
-from githubdata import GithubData
+from mirutil.latex import ret_pathes_4_latex_dirtree_forest
 
 
+class Param :
+    root_dir = '/Users/mahdi/Library/CloudStorage/OneDrive-khatam.ac.ir/Heidari Data/V2'
+    gitignore_file = '/Users/mahdi/Library/CloudStorage/OneDrive-khatam.ac.ir/Heidari Data/V2/.gitignore'
 
+p = Param()
 
-def main():
+def main() :
     pass
 
     ##
+    fu = ret_pathes_4_latex_dirtree_forest
+    res = fu(p.root_dir , p.gitignore_file , only_dirs = True)
+    print(res)
 
     ##
+    with open('res.txt' , 'w') as fi :
+        fi.write(res)
+        print('saved as res.txt')
 
     ##
 
 ##
-if __name__ == "__main__":
+if __name__ == "__main__" :
     main()
-
-##
-# noinspection PyUnreachableCode
-if False:
-    pass
-
-    ##
-
-
-    ##
-
-
-    ##
-
-##
+    print('Done!')
